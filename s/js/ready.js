876a3f1f38ba8943d8ed9b3f81090a44460b1243
Andrew Chilton <chilts@appsattic.com>
2012-04-27 17:37:03 +1200

    Add mailto links obfuscated with JS

---
+++ s/js/ready.js
@@ -0,0 +1,9 @@
+$(function() {
+
+    // fix up the mailto links
+    var $mailto = $('.mailto').;
+    var email = $('.mailto').first().text().split('').reverse().join('');
+    $mailto.attr('href', 'mailto:' + email);
+    $mailto.html(email);
+
+});
