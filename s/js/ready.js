c8e2f31aca6462510a04254db10334f1a8bbc631
Andrew Chilton <chilts@appsattic.com>
2012-04-27 17:38:45 +1200

    Minor fixes to mailto links

--- s/js/ready.js
+++ s/js/ready.js
@@ -1,8 +1,8 @@
 $(function() {
 
     // fix up the mailto links
-    var $mailto = $('.mailto').;
-    var email = $('.mailto').first().text().split('').reverse().join('');
+    var $mailto = $('.mailto');
+    var email = $mailto.first().text().split('').reverse().join('');
     $mailto.attr('href', 'mailto:' + email);
     $mailto.html(email);
 
