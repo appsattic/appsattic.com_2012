e5b84e352ea2530d418ef9d00d542fd8733218b0
Andrew Chilton <andychilton@gmail.com>
2016-07-19 15:30:37 +1200

    Remove the old stuff

--- s/js/ready.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-
-    // fix up the mailto links
-    var $mailto = $('.mailto');
-    var email = $mailto.first().text().split('').reverse().join('');
-    $mailto.attr('href', 'mailto:' + email);
-    $mailto.html(email);
-
-});
